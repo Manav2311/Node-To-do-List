c336ed2ab1186ff25fc08b1814c05e8d28bedaa0
Manav Prajapati <154076260+Manav2311@users.noreply.github.com>
2024-09-06 22:26:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require('express')
 
-const port = 8081;
+const port = 8081
 
-let app = express();
+let app = express()
 
 app.set('view engine', 'ejs');
 
@@ -10,46 +10,41 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-let lists = [];
+let lists =[]
 
 app.get('/', (req, res) => {
     return res.render('index', {
         lists
     });
-});
+})
 
-app.post('/insertData', (req, res) => {
-    let { id, task, editId } = req.body;
-    
-    // Validation: check if task is not empty
-    if (!task || task.trim() === '') {
-        return res.redirect('/');
-    }
-
+app.post('/insertData',(req,res)=>{
+    let {id,task,editId}= req.body
     if (editId) {
         let data = lists.map((val) => {
             if (val.id == editId) {
                 val.task = task;
             }
             return val;
-        });
+        })
         lists = data;
-    } else {
-        lists.push({ id, task });
+    }
+    else {
+        lists.push({id, task});
     }
 
     return res.redirect('/');
-});
+})
 
 app.get('/deleteData/:id', (req, res) => {
     let { id } = req.params;
     console.log(id);
     let data = lists.filter((list) => {
         return list.id != id;
-    });
-    lists = data;
+    })
+    lists = data
     return res.redirect('/');
-});
+})
 
 app.get('/editData/:id', (req, res) => {
     let { id } = req.params;
@@ -63,10 +58,12 @@ app.get('/editData/:id', (req, res) => {
     return res.render('edit', {
         data: data[0]
     });
-});
 
-app.listen(port, (err) => {
-    if (!err) {
-        console.log("server start on http://localhost:" + port);
+})
+
+
+app.listen(port,(err)=>{
+    if(!err){
+        console.log("server start on http://localhost:"+port);
     }
-});
+})
